79bb7439351e899c47424f6025103da2c1bdc378
Jon Lara <jon.laratrigo@gmail.com>
2024-01-10 12:40:37 +0100

    add test convert url encoded

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -5,4 +5,10 @@ describe("Utils", () => {
     const str = objectToParams({ test: "test", test2: "test2" });
     expect(str).toEqual("test=test&test2=test2");
   });
+
+  test("Convert object to params no url encoded", () => {
+    const str = objectToParams({ test: "&s%/agf", "test%sa/S": "%)a" });
+    // should convert to url encoded
+    expect(str).toEqual(encodeURIComponent("test") + "=" + encodeURIComponent("&s%/agf") + "&" + encodeURIComponent("test%sa/S") + "=" + encodeURIComponent("%)a"));
+  });
 });
